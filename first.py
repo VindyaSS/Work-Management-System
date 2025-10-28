e6f8d504afe088c76853f45195b4f73784444ea6
Vindya <indunilvindya2000@gmail.com>
2025-10-28 07:46:41 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,23 @@
+
+
+print("+--------------------------To Do List-------------------------------------------+")
+
+print("|                      1. Add list                                              |")
+print("|                      2. View List                                             |")
+print("|                      3. Remove list                                           |")
+
+print("+-------------------------------------------------------------------------------+")
+
+
+
+x = int(input("enter the number what do you want:"))
+
+if(x==1):
+    
+    List =input("adding tasks:")
+elif(x==2):
+    List=input("view task:")
+elif(x==3):
+    List=input("delete task:")
+
+
