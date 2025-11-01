5a39e28d9ca7191d0b44f4a50ff080e947c2b70f
Vindya <indunilvindya2000@gmail.com>
2025-11-02 00:07:33 +0530

    Update first.py

--- first.py
+++ first.py
@@ -8,17 +8,24 @@ print("|                      3. Remove list                                    
 
 print("+-------------------------------------------------------------------------------+")
 
-task = []
+tasks = []
 
 while True:
     x = int(input("enter the number what do you want:"))
     if x==1:
         while True:
             task =input("adding tasks:")
+            tasks.append(task)
             y = input("Do u want to add another task? yes/no:")
             if y=='no':
                 break
-    break      
-    
+         
+    if x==2:
+        for i in range(len(tasks)):
+            print(i+1,tasks[i])
+
+    if x==3:
+        z = input("what task do u want to remove?:")        
+
 
 
