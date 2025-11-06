9eae54de665ae664d1c21125d98e2eebb7baa0b2
Vindya <indunilvindya2000@gmail.com>
2025-11-06 18:19:19 +0530

    Update first.py

--- first.py
+++ first.py
@@ -5,6 +5,7 @@ print("+--------------------------To Do List------------------------------------
 print("|                      1. Add list                                              |")
 print("|                      2. View List                                             |")
 print("|                      3. Remove list                                           |")
+print("|                      4. Exit                                                         |")
 
 print("+-------------------------------------------------------------------------------+")
 
@@ -17,15 +18,23 @@ while True:
             task =input("adding tasks:")
             tasks.append(task)
             y = input("Do u want to add another task? yes/no:")
-            if y=='no':
+            if y!='yes':
                 break
          
     if x==2:
-        for i in range(len(tasks)):
-            print(i+1,tasks[i])
+        for z in range(len(tasks)):
+            print(z+1,tasks[z])
 
     if x==3:
-        z = input("what task do u want to remove?:")        
+        z = int(input("Enter the task number to remove: "))
+        if 1<= z <=len(tasks):        
+            tasks.pop(z-1)
 
+    if x==4:
+        print("Goodbye! 👋")
+        break
+        
+        
+            
 
 
